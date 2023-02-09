a86a8b962cd53bf6674011b3eb5a1bacecb63fa6
Ilham Yori P <51188704+ilham-yori@users.noreply.github.com>
2023-02-09 13:24:53 +0700

    Setup ML API

--- API-ML/ginjalAPI.py
+++ API-ML/ginjalAPI.py
@@ -1,53 +1,67 @@
-import io
+import cv2
+import numpy as np
 import torch
+import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+from io import BytesIO
 from PIL import Image
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
+from torchvision import transforms
 
 app = Flask(__name__)
-classes = ['Batu_Ginjal', 'Normal']
-model = torch.load("C:/Users/User/Downloads/3D/stage_2.kidney.pth")
 
-# Define the image pre-processing transform
-transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+# Load the saved model
+checkpoint_path='C:/laragon/www/lab-ginjal-app/API-ML/MLModel/model.pt'
+model= torchvision.models.resnet18(pretrained=True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
+model.load_state_dict(torch.load(checkpoint_path,map_location='cpu'),strict=False)
+model.eval()
 
-# Route for predictions
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the image from the request
-    print("Before First")
-    image_data = request.files.get('image').read()
-    print("Get Image")
-    pil_img = Image.open(io.BytesIO(image_data))
-    print("27")
-    
-    # Pre-process the image
-    image = transform(pil_img).unsqueeze(0)
-    print("31")
-    
+
+def preprocess_image(image):
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    image = transform(image).unsqueeze(0)
+    return image
+
+# Define a function for making predictions with the model
+def predict(image):
+    # Apply preprocessing to the image
+    #image = preprocess_image(image)
+
     # Make a prediction with the model
+    image = image.unsqueeze(0)
+    print("Predict")
     with torch.no_grad():
-        print("Atas Output")
         output = model(image)
-        print("Bawah Output")
-        prediction = output.argmax().item()
-        
-    print("37")
-        
-    # Return the prediction in the response
-    print("Before Return")
-    return jsonify({'class': 'normal kidney' if prediction == 0 else 'kidney stone'})
+        _, prediction = torch.max(output, 1)
+    return prediction.item()
 
+# Route for the main page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def home():
-    return f"Running Flask on Google Colab!"
+# Route for uploading an image
+@app.route('/predict', methods=['POST'])
+def predict2():
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    image = Image.open(BytesIO(request.files['image'].read()))
+    image = transform(image).unsqueeze(0)
+    output = model(image)
+    prediction = output.argmax().item()
+    print(output)
+    return str(prediction)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-    
