dbec0191a2e5a731078d32d851f5400de7a9088a
Ilham Yori P <51188704+ilham-yori@users.noreply.github.com>
2023-06-26 13:05:59 +0700

    ML Model Change & Clean Code

--- API-ML/ginjalAPI - Copy - Copy.py
+++ API-ML/ginjalAPI - Copy - Copy.py
@@ -10,40 +10,31 @@ from PIL import Image
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from torchvision import transforms
+from numpy import asarray
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
 
 # Load the saved model
-checkpoint_path='C:/laragon/www/lab-ginjal-app/API-ML/MLModel/newmodel.pt'
+checkpoint_path='C:/laragon/www/lab-ginjal-app/API-ML/MLModel/newModelBismillahFinal.pt'
 model= resnet50(weights=ResNet50_Weights.DEFAULT)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 2)
 model.load_state_dict(torch.load(checkpoint_path,map_location='cpu'),strict=False)
 model.eval()
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def preprocess_image(image):
-    transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    image = transform(image).unsqueeze(0)
-    return image
-
-# Define a function for making predictions with the model
-def predict(image):
-    # Apply preprocessing to the image
-    #image = preprocess_image(image)
-
-    # Make a prediction with the model
-    image = image.unsqueeze(0)
-    print("Predict")
-    with torch.no_grad():
-        output = model(image)
-        _, prediction = torch.max(output, 1)
-    return prediction.item()
+# CLAHE Pre-Process
+def apply_clahe(img):
+    data = asarray(img)
+    gray_image = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=8.0)
+    enhanced_image = clahe.apply(gray_image)
+    colorimg = cv2.cvtColor(enhanced_image, cv2.COLOR_GRAY2BGR)
+    image2 = Image.fromarray(colorimg)
+    return image2
 
 # Route for the main page
 @app.route('/')
@@ -54,15 +45,19 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict2():
     transform = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((224, 224)),
+        transforms.Lambda(apply_clahe),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
+
     image = Image.open(BytesIO(request.files['image'].read()))
     image = transform(image).unsqueeze(0)
     output = model(image)
     prediction = output.argmax().item()
+
     print(output)
+
     return str(prediction)
 
 if __name__ == '__main__':
